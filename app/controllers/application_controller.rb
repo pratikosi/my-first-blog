f02a88590aa66c6ff75f043410233c8799c0885a
Pratik Oswal <pratikosi@gmail.com>
2017-04-04 01:39:37 +0000

    initial blog commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  include CanCan::ControllerAdditions
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:notice] = "Access denied!"
+    flash.keep(:notice)
+    redirect_to posts_path
+  end
+end
